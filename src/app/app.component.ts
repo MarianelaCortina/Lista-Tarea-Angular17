2ce7b80f70e7d2f3765e7dac6e434e60196fe4a6
Marianela Cortina <marianelacort@hotmail.com>
2024-09-06 11:31:22 -0300

    Add task list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,40 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { TareasService } from './services/tareas.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  
   title = 'listaTareasApp';
+
+  listaTareas: string[] = [];
+  nuevaTarea:string = '';
+
+  private _tareaService = inject(TareasService);
+
+
+  ngOnInit(): void {
+    this.listaTareas = this._tareaService.getListaTareas();
+  }
+
+  agregarTarea(){
+    this._tareaService.agregarTarea(this.nuevaTarea);
+    this.nuevaTarea = '';
+    this.listaTareas = this._tareaService.getListaTareas();
+  }
+
+  eliminarTarea(index: number){
+    this._tareaService.eliminarTarea(index);
+    this.listaTareas = this._tareaService.getListaTareas();
+
+  }
+
 }
